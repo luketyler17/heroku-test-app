e89272af89719c5f57362434137e1c507057b8c4
Luke Tyler <luketyler17@gmail.com>
2022-09-29 15:54:44 -0400

    all

---
+++ src/components/IndividualItems.jsx
@@ -0,0 +1,32 @@
+import { Card, Paper } from "@mui/material"
+import {FiDelete} from 'react-icons/fi'
+import {GrDocumentUpdate} from 'react-icons/gr'
+import {Button} from "@mui/material"
+const IndividualItems = ({ item }) => {
+    if(item.Description.length > 100) {
+        let newdescription = item.Description.slice(0, 100);
+        newdescription += '...';
+        item.Description = newdescription
+    }
+    console.log(item)
+    return(
+            <Card variant='outlined'
+                style={{
+                    display: 'flex',
+                    width:"25%",
+                    height: "150px",
+                    flexWrap: 'wrap',
+                    margin: '15px',
+                }}>
+                    Item Name: {item.ItemName}
+                    <br />
+                    Quantity: {item.Quantity}
+                    <br />
+                    Description: {item.Description}
+                    <Button variant="outlined">Update</Button>
+                    <Button variant="outlined">Delete</Button>
+            </Card>
+    )
+}
+
+export default IndividualItems
---
+++ src/components/header.jsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+
+
+export default function Header() {
+    const cookies = new Cookies();
+    var token = cookies.get('input')
+    let navigate = useNavigate()
+    const navigateLoginHandle = () => {
+        navigate('/login')
+    }
+    const navigateHome = () => {
+        navigate('/');
+    }
+    const handleDetails = () => {
+        navigate('/details')
+    }
+    const removeCookie = () => {
+        cookies.remove('input', {path: '/'})
+        navigate('/')
+        window.location.reload(true);
+    }
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+        <Button color="inherit"
+          onClick={navigateHome}
+          >Home</Button>
+          {token == undefined ? (<Button color="inherit"
+          onClick={navigateLoginHandle}
+          >Login</Button>) : (<><Button color="inherit" onClick={handleDetails}>Details</Button><Button color="inherit" onClick={removeCookie}>LOG OUT</Button></>)}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
