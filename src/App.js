2a704bbe06794d56dbdfe5f9f5a5a22c594ce11c
Luke Tyler <luketyler17@gmail.com>
2022-09-29 16:06:22 -0400

    all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import ViewDetails from './pages/ViewDetails';
