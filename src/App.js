3b2b3c5f1016e8cb92255c7fc9a4dd318b202d05
Luke Tyler <luketyler17@gmail.com>
2022-09-29 16:18:09 -0400

    functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './components/homepage';
 function App() {
   return (
     <>
